946154d2666d6179e50aead7eac7e503326f51a1
chayansrk64 <chayansrk64@gmail.com>
2023-03-30 11:06:42 +0600

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <h1 className='text-center text-3xl font-bold text-red-500'>Demo Assingment</h1>
+      <h2 className='text-3xl text-red-600'>Testing after deployed in netlify</h2>
     </div>
   )
 }
