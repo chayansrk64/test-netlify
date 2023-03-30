c5387c09bdd2e272f7011d85d6389b026266a093
chayansrk64 <chayansrk64@gmail.com>
2023-03-30 10:58:15 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1 className='text-center text-3xl font-bold text-red-500'>Demo Assingment</h1>
+    </div>
+  )
+}
+
+export default App
